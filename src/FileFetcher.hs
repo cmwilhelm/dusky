38dc683f421af0423c42e7eb8ca9cf11e3ab0759
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-01-03 17:56:17 -0800

    Utility to fetch forecast images for sunrise and sunset

---
+++ src/FileFetcher.hs
@@ -0,0 +1,73 @@
+module FileFetcher where
+
+import Control.Monad
+import qualified Data.ByteString as B
+import Network.HTTP
+import Network.URI (parseURI)
+
+
+data ForecastType  = Sunrise | Sunset deriving (Show, Eq)
+data ForecastImage = ForecastImage { forecastType :: ForecastType
+                                   , imageUrl     :: String
+                                   , fileName     :: String
+                                   , time         :: Int
+                                   } deriving (Show, Eq)
+
+
+baseUrl :: String
+baseUrl = "http://sunsetwx.com/"
+
+
+sunriseImages :: [ForecastImage]
+sunriseImages = map makeImage [8..23]
+  where buildUrl zTime      =  baseUrl
+                            ++ "sunrise/sunrise_f"
+                            ++ show (zTime - 7)
+                            ++ ".png"
+
+        buildFileName zTime =  "sunrise_f"
+                            ++ show (zTime - 7)
+                            ++ ".png"
+
+        makeImage zTime     = ForecastImage { forecastType = Sunrise
+                                            , imageUrl     = buildUrl zTime
+                                            , fileName     = buildFileName zTime
+                                            , time         = zTime }
+
+
+sunsetImages :: [ForecastImage]
+sunsetImages = map makeImage [20..36]
+  where buildUrl zTime      =  baseUrl
+                            ++ "sunset/sunset_f"
+                            ++ show (zTime - 18)
+                            ++ ".png"
+
+        buildFileName zTime =  "sunset_f"
+                            ++ show (zTime - 18)
+                            ++ ".png"
+
+        makeImage zTime     = ForecastImage { forecastType = Sunset
+                                            , imageUrl     = buildUrl zTime
+                                            , fileName     = buildFileName zTime
+                                            , time         = zTime }
+
+
+get :: String -> IO B.ByteString
+get url = let uri = case parseURI url of
+                      Nothing -> error $ "Invalid URI: " ++ url
+                      Just u -> u in
+            simpleHTTP (defaultGETRequest_ uri) >>= getResponseBody
+
+
+fetchImage :: ForecastImage -> IO ()
+fetchImage (ForecastImage _ url fName _) = do
+  png <- get url
+  B.writeFile fName png
+
+
+fetchSunriseImages :: IO ()
+fetchSunriseImages = forM_ sunriseImages fetchImage
+
+
+fetchSunsetImages :: IO ()
+fetchSunsetImages = forM_ sunsetImages fetchImage
