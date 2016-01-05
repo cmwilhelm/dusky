c9d4ea361d048f0f7ea3feac8b0413ea1aed0211
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-01-04 22:29:16 -0800

    Lock chart height to 100, show points on line

--- src/Charter.hs
+++ src/Charter.hs
@@ -1,5 +1,6 @@
 module Charter (renderAndSaveLineGraph) where
 
+import Graphics.Rendering.Chart
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Diagrams
 
@@ -11,7 +12,17 @@ makeLineGraph dataPoints graphName = toRenderable layout
           layout_background .= solidFillStyle (opaque white)
           layout_foreground .= (opaque black)
 
+          let locker = if length dataPoints > 0
+                         then (fst $ head dataPoints, 100)
+                         else (0, 100)
+
           plot (line "Intensity" [dataPoints])
+          plot (line "" [[locker]])
+
+          plot $ liftEC $ do
+            plot_points_values .= dataPoints
+            plot_points_title  .= ""
+            plot_points_style . point_radius .= 3
 
 
 renderAndSaveLineGraph :: [(Int, Int)] -> String -> String -> IO (PickFn ())
